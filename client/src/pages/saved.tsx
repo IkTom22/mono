75a775659a4d3eb422c500d3a4433159fd157ead
Iko <ikotominaga@gmail.com>
2024-04-26 18:21:35 +1200

    favouriteLists are shouwing

--- client/src/pages/saved.tsx
+++ client/src/pages/saved.tsx
@@ -1,7 +1,43 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 function Saved() {
   const [name, setName] = useState('');
+  const [favouriteLists, setFavouriteLists] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const router = useRouter();
+  useEffect(() => {
+    async function checkUser() {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/check_user`, {
+        credentials: 'include',
+      });
+      const userCheck = await res.json();
+      if (userCheck.logged_in) {
+        setIsLoggedIn(true);
+      } else {
+        console.log(userCheck);
+        router.push('/');
+      }
+    }
+    checkUser();
+  }, []);
+  useEffect(() => {
+    async function getFavouriteLists() {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/favourite_lists`,
+        {
+          credentials: 'include',
+        },
+      );
+      const fetchedFavouriteLists = await res.json();
+      console.log('give me the listing', fetchedFavouriteLists);
+      setFavouriteLists(fetchedFavouriteLists);
+    }
+    if (isLoggedIn) {
+      getFavouriteLists();
+    }
+  }, [isLoggedIn]);
+  console.log('favouriteLists is.. ', favouriteLists);
   console.log('name is , ', name);
   function handleCreate() {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/favourite_lists`, {
@@ -32,6 +68,11 @@ function Saved() {
         onChange={(e) => setName(e.target.value)}
       />
       <button onClick={handleCreate}>Create</button>
+      <div>
+        {favouriteLists.map((fav) => (
+          <p>{fav.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
