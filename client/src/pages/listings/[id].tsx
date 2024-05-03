ec2b6bfb7f6dfddc25a0d80311cc2bd2f97f0367
Iko <ikotominaga@gmail.com>
2024-05-03 17:06:49 +1200

    shad/cn setted up and modal is added to the ui component

--- client/src/pages/listings/[id].tsx
+++ client/src/pages/listings/[id].tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { GetStaticPaths } from 'next';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/Dialog';
 
 function ListingPage(props: any) {
-  console.log(props);
+  console.log('Is this the props we need ...? ', props);
   const router = useRouter();
   console.log(router.query);
-  return <div>[id]</div>;
+  return (
+    <div>
+      <h1>{props.listing.name}</h1>
+      <Dialog>
+        <DialogTrigger>Add to favourite</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
 
 export default ListingPage;
